9a018d82ea42225c85aa10f36d880d34d5a67c81
Matthew Grinnell <matthew.grinnell@dfo-mpo.gc.ca>
2020-05-21 09:16:47 -0700

    Add Fresh.bat

---
+++ Fresh.bat
@@ -0,0 +1,24 @@
+# Clean up the directory after a LaTeX build (Windows version)
+del *.aux
+del *.bbl
+del *.blg
+del *.dvi
+del *.log
+del *.lof
+del *.lot
+del *.nav
+del *.out
+del *.ps
+del *.snm
+del *.toc
+del *.vrb
+del *-concordance.tex
+del *.synctex.gz*
+del DataSummary.tex
+del DataSummary.pdf
+del Presentation.pdf
+del Presentation.tex
+del .Rhistory
+rmdir /S /Q knitr-cache
+rmdir /S /Q figure
+rmdir /S /Q cache
